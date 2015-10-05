645bc09fb0a356ddd39823444ce0a7fabf33b3a8
Eduardo Neves Córdova <eduucordova@gmail.com>
2015-10-04 22:08:44 -0300

    new features

--- Formais/regular_sets/finite_automaton.py
+++ Formais/regular_sets/finite_automaton.py
@@ -1,39 +1,42 @@
-﻿
-from functools import reduce
+﻿from functools import reduce
+from regular_grammar import regular_grammar
 
-
-def get_alphabet(machine):
-    """Returns the NFA's or DFA's input alphabet, generated on the fly.
-    execute 'or (union)' foreach two set elements from simbols
-
-    :param machine: {dict} NFA or DFA
-    :return:{set} alphabet
-    """
-    sigma = reduce(
-        (lambda a, b: set(a) | set(b)),
-        [x.keys() for x in machine.delta.values()]
-    )
-    return sigma
-
+def _union_sets(self, x, y):
+        if (len(x) or len(y)):
+            return (x | y )
 
 class DFA:
     """Class that encapsulates a DFA."""
 
     def __init__(self, delta_transitions, initial_state, accept_states):
+        """DFA Constructor
+        :param delta_transitions: {dict} transitions
+            ex. {'q0': {'a': {'q1'}, 'b': {'q1'}}, 'q1': {'a': {'q0'}}}
+        :param initial_state: {str}
+        :param accept_states: {list} with {str} accept states
+        """
         self.delta = delta_transitions
         self.initial_state = initial_state
         self.accept_states = set(accept_states)
 
     def compute(self, state, input_string):
         """ compute strings in DFA
-        :param state: is actual state
+        :param state: is current state
         :param input_string: string to compute
         :return: last state computed
         """
-        state = {state}
-        for a in input_string:
-            state = self.delta[next(iter(state))][a]
-        return next(iter(state))
+        _state = state
+
+        try:
+            if (not isinstance(_state, frozenset)):
+                for a in input_string:
+                    _state = next(iter(self.delta[_state][a]))
+            else:
+                for a in input_string:
+                    _state = self.delta[_state][a]
+        except KeyError:
+            return {'reject'}
+        return _state
 
     def validate_sentence(self, input_string):
         """
@@ -43,20 +46,145 @@ class DFA:
         """
         return self.compute(self.initial_state, input_string) in self.accept_states
 
+    def get_alphabet(self):
+        """Returns the NFA's or DFA's input alphabet, generated on the fly.
+        execute 'or (union)' between foreach two elements set from symbols of the machine
+
+        :return: {set} alphabet
+        """
+        sigma = reduce(
+            (lambda a_symbol, b_symbol: set(a_symbol) | set(b_symbol)),
+            [dicts.keys() for dicts in self.delta.values()])
+
+        return sigma
+
+    def get_states(self):
+        """Returns the NFA's or DFA's input states, generated on the fly.
+        execute 'or union' between foreach two states set from machine
+
+        :return: {set} states
+        """
+        states_list = reduce((lambda a_list, b_list: a_list + b_list),
+                             [list(symbols.values()) for symbols in self.delta.values()])
+
+        _states = {self.initial_state} | set(self.delta.keys()) | reduce((lambda a, b: a | b), states_list)
+
+        return _states
+
     def to_grammar(self):
         """
         :return: Productions of grammar
         """
+        terminals = self.get_alphabet()
+        initial_simbol = self.initial_state
+        non_terminals = set()
         productions = dict()
         temp = list()
+        string = ''
+        next_state = set()
 
         for current_state in self.delta: #each state
+            non_terminals.add(current_state)
             for alphabet_simbol in self.delta[current_state]: #each simbol of the alphabet
-                for next_state in self.delta[current_state][alphabet_simbol]: #each next state
-                    if next_state in self.accept_states:
+                for next_state_simbol in self.delta[current_state][alphabet_simbol]: #each next state
+                    string += next_state_simbol
+                    next_state.add(next_state_simbol)
+                    if next_state in self.accept_states and alphabet_simbol not in temp:
                         temp.append(alphabet_simbol)
-                    temp.append(alphabet_simbol + next_state)
+                temp.append(alphabet_simbol + string)
+                string = ''
             productions[current_state] = temp
             temp = list()
+            next_state = set()
 
-        return productions
+        return regular_grammar(non_terminals, terminals, productions, initial_simbol)
+
+class NDFA(DFA):
+    """Class that encapsulates an NFA.
+    inherit from DFA
+    """
+
+    def compute(self, state, input_string):
+        """override compute from {DFA}
+        compute strings in NDFA
+        :param state: is current state
+        :param input_string: string to compute
+        :return: empty set if transition isn't defined
+                else set with states
+        """
+        _states = {state}
+        for a in input_string:
+            new_states = set([])
+            for _state in _states:
+                try:
+                    new_states |= self.delta[_state][a]
+                except KeyError:
+                    pass
+            _states = new_states
+        return _states
+
+    def validate_sentence(self, input_string):
+        """override validate_sentence from {DFA}
+        :param input_string: sentence to validate
+        :return: True, if is a valid sentence
+                 False, otherwise
+        """
+        return len(self.compute(self.initial_state, input_string) & self.accept_states) > 0
+
+    def determinization(self):
+        """Converts the input NFA into a DFA.
+        :return: {DFA} compatible with this NDFA
+        """
+        closure = {_state: self.epsilon_closure(_state) for _state in self.get_states()}
+
+        _initial_state = frozenset(closure[self.initial_state])  # define _initial_state as immutable
+        _states = {_initial_state}
+        unprocessed_states = _states.copy()  # unprocessed_states tracks states for which delta is not yet defined
+        new_delta = {}
+        accept_states = []
+        sigma = self.get_alphabet()
+
+        while unprocessed_states:
+            current_state = unprocessed_states.pop()
+            new_delta[current_state] = {}
+            for symbol in sigma:
+                if symbol == '&':
+                    continue
+                next_states_list = [self.compute(q, symbol)
+                                    for q in current_state
+                                    if len(self.compute(q, symbol))]  # it gets list of next states
+                try:
+                    next_states = reduce(lambda x, y: _union_sets(closure[x], closure[y]), next_states_list) # union foreach list elements
+                except TypeError:
+                    next_states = set([])
+
+                next_states = frozenset(next_states)
+                if len(next_states):
+                    new_delta[current_state][symbol] = next_states
+
+                    if not next_states in _states:  # if new state found, add to unprocessed list for it processes in future
+                        _states.add(next_states)
+                        unprocessed_states.add(next_states)
+
+        for current_state in _states:
+            if len(current_state & self.accept_states) > 0:
+                accept_states.append(current_state)
+
+        return DFA(new_delta, _initial_state, accept_states)
+
+    def epsilon_closure(self, state):
+        epsilon_transitions = {_state: self.delta[_state]['&']
+                               for _state, symbol in self.delta.items()
+                               if '&' in symbol}
+        unprocessed_states = {state}
+        _epsilon_closure = {state}
+
+        while unprocessed_states:
+            current_state = unprocessed_states.pop()
+            if current_state in epsilon_transitions:
+                state_to_add = {_state for _state in epsilon_transitions[current_state]
+                                if _state not in _epsilon_closure}
+                _epsilon_closure |= state_to_add
+                unprocessed_states.add(s for s in state_to_add)
+
+        return _epsilon_closure
--- Formais/regular_sets/finite_automaton_test.py
+++ Formais/regular_sets/finite_automaton_test.py
@@ -1,13 +1,12 @@
-﻿import unittest
+import unittest
+import sys
 
-import finite_automaton
-from finite_automaton import DFA
+from regular_sets.finite_automaton import DFA, NDFA
 
 class DFATests(unittest.TestCase):
     def setUp(self):
         """delta table
-
-           L = {w | w ∈ Σ⋆={a, b} and |w| is pair}
+           L = {w | w ∈ Σ*={a, b} and |w| is pair}
 
            delta |   a   |   b   |
            ------|-------|-------|
@@ -16,30 +15,264 @@ class DFATests(unittest.TestCase):
            ----------------------|
         """
         self.delta = {'q0': {'a': {'q1'}, 'b': {'q1'}}, 'q1': {'a': {'q0'}, 'b': {'q0'}}}
-        self.dfa = DFA(self.delta, 'q0', ['q0'])
 
-    def test_accept_sentence(self):
+        initial_state = 'q0'
+        accept_states = ['q0']
+        self.dfa = DFA(self.delta, initial_state, accept_states)
+
+    def test_accept_sentence_abab(self):
         """
         DFA accept sentence 'abab'
         """
         is_accept = self.dfa.validate_sentence('abab')
         self.assertTrue(is_accept)
 
-    def test_reject_sentence(self):
+    def test_reject_sentence_ababa(self):
         """
         DFA reject sentence 'ababa'
         """
         is_accept = self.dfa.validate_sentence('ababa')
         self.assertFalse(is_accept)
 
-    def test_get_alphabet(self):
+    def test_get_alphabet_from_DFA(self):
         """
         get alphabeth from DFA
         alphabeth = {a, b}
         """
-        #print()
-        sigma = finite_automaton.get_alphabet(self.dfa)
+        sigma = self.dfa.get_alphabet()
         self.assertSetEqual({'a', 'b'}, sigma)
+
+    def test_get_states_from_DFA(self):
+        """
+        get states from DFA
+        states {q0, q1}
+        """
+        states = self.dfa.get_states()
+        self.assertSetEqual({'q0', 'q1'}, states)
+
+
+class NDFATests(unittest.TestCase):
+    def setUp(self):
+        """delta table
+           L = { a+ or (ab)+ }
+
+           delta |    a   |   b   |
+           ------|--------|-------|
+            ->q0 | q1,q2  |   -   |
+             *q1 |   q1   |   -   |
+              q2 |   -    |  q3   |
+             *q3 |   q2   |   -   |
+           -----------------------|
+        """
+        self.delta = {
+            'q0': {'a': {'q1', 'q2'}},
+            'q1': {'a': {'q1'}},
+            'q2': {'b': {'q3'}},
+            'q3': {'a': {'q2'}}
+        }
+
+        initial_state = 'q0'
+        accept_states = ['q1', 'q3']
+        self.ndfa = NDFA(self.delta, initial_state, accept_states)
+
+    def create_epsilon_ndfa(self):
+        """ delta table
+            L = { #a=pair xor #b=par }
+            delta |    a   |   b   |   &   |
+            ------|--------|-------|-------|
+             ->q0 |    -   |   -   | q1,q3 |
+              *q1 |   q2   |   -   |   -   |
+               q2 |   q1   |   -   |   -   |
+              *q3 |    -   |   q4  |   -   |
+               q4 |    -   |   q3  |   -   |
+            -------------------------------|
+
+        :return:{NDFA} non epsilon free
+        """
+        initial_state = 'q0'
+        accept_states = ['q1', 'q3']
+
+        delta_with_epsilon = {
+            'q0': {'&': {'q1', 'q3'}},
+            'q1': {'a': {'q2'}},
+            'q2': {'a': {'q1'}},
+            'q3': {'b': {'q4'}},
+            'q4': {'b': {'q3'}}
+        }
+
+        return NDFA(delta_with_epsilon, initial_state, accept_states)
+
+    def expected_dfa(self):
+        """ create DFA to help the tests
+            new_delta |     a    |     b    |
+           -----------|----------|----------|
+              ->{q0}  | {q1,q2}  |     -    |
+              *{q1,q2}|   {q1}   |   {q3}   |
+               *{q1}  |   {q1}   |     -    |
+               *{q3}  |   {q2}   |     -    |
+                {q2}  |     -    |   {q3}   |
+           ---------------------------------|
+        :return: {DFA}
+        """
+        # It's needed use frozenset on states to iterate by element in DFA
+        # for not use iterate in hashes
+        delta = {frozenset({'q0'}): {'a': frozenset({'q1','q2'})},
+                 frozenset({'q1', 'q2'}): {'a': frozenset({'q1'}), 'b': frozenset({'q3'})},
+                 frozenset({'q1'}): {'a': frozenset({'q1'})},
+                 frozenset({'q3'}): {'a': frozenset({'q2'})},
+                 frozenset({'q2'}): {'b': frozenset({'q3'})}}
+
+        initial_state = frozenset({'q0'})
+        accept_states = [frozenset({'q1', 'q2'}),
+                         frozenset({'q1'}),
+                         frozenset({'q3'})]
+
+        return DFA(delta, initial_state, accept_states)
+
+    def expected_dfa_epsilon_free(self):
+        """ create DFA to help the tests
+             new_delta  |   a  |   b  |
+           -------------|------|------|
+           ->*{q0,q1,q3}| {q2} | {q4} |
+                    {q2}| {q1} |   -  |
+                    {q4}|   -  | {q3} |
+                   *{q1}| {q2} |   -  |
+                   *{q3}|   -  | {q4} |
+           ---------------------------|
+        :return: {DFA}
+        """
+        # It's needed use frozenset on states to iterate by element in DFA
+        # for not use iterate in hashes
+        delta = {frozenset({'q0','q1','q3'}): {'a': frozenset({'q2'}), 'b': frozenset({'q4'})},
+                 frozenset({'q2'}): {'a': frozenset({'q1'})},
+                 frozenset({'q4'}): {'b': frozenset({'q3'})},
+                 frozenset({'q1'}): {'a': frozenset({'q2'})},
+                 frozenset({'q3'}): {'b': frozenset({'q4'})}}
+
+        initial_state = frozenset({'q0','q1','q3'})
+        accept_states = [frozenset({'q0','q1','q3'}),
+                         frozenset({'q1'}),
+                         frozenset({'q3'})]
+
+        return DFA(delta, initial_state, accept_states)
+
+    def test_accept_sentence_aaa(self):
+        """
+        DFA accept sentence 'aaa'
+        """
+        is_accept = self.ndfa.validate_sentence('aaa')
+        self.assertTrue(is_accept)
+
+    def test_accept_sentence_ababab(self):
+        """
+        DFA accept sentence 'ababab'
+        """
+        is_accept = self.ndfa.validate_sentence('ababab')
+        self.assertTrue(is_accept)
+
+    def test_reject_sentence_aaabab(self):
+        """
+        DFA reject sentence 'aaabab'
+        """
+        is_accept = self.ndfa.validate_sentence('aaabab')
+        self.assertFalse(is_accept)
+
+    def test_get_alphabet_from_NDFA(self):
+        """
+        get alphabet from DFA
+        alphabet = {a, b}
+        """
+        sigma = self.ndfa.get_alphabet()
+        self.assertSetEqual({'a', 'b'}, sigma)
+
+    def test_get_states_from_NDFA(self):
+        """
+        get states from DFA
+        states {q0, q1}
+        """
+        states = self.ndfa.get_states()
+        self.assertSetEqual({'q0', 'q1', 'q2', 'q3'}, states)
+
+    def test_determinization(self):
+        """new delta table determinate
+
+            transform this NDFA to compatible DFA
+            L = { a+ or (ab)+ }
+
+            delta |    a   |   b   |             new_delta |     a    |     b    |
+            ------|--------|-------|              ---------|----------|----------|
+             ->q0 | q1,q2  |   -   |       \       ->{q0}  | {q1,q2}  |     -    |
+              *q1 |   q1   |   -   |    ----\      *{q1,q2}|   {q1}   |   {q3}   |
+               q2 |   -    |  q3   |    ----/       *{q1}  |   {q1}   |     -    |
+              *q3 |   q2   |   -   |       /        *{q3}  |   {q2}   |     -    |
+            -----------------------|                 {q2}  |     -    |   {q3}   |
+                                                  -------------------------------|
+        """
+        ndfa_determinized = self.ndfa.determinization()
+
+        self.assertIsInstance(ndfa_determinized, DFA)
+
+        expected_dfa = self.expected_dfa()
+        self.assertDictEqual(expected_dfa.delta, ndfa_determinized.delta) # check delta transitions
+        self.assertSetEqual(expected_dfa.accept_states, ndfa_determinized.accept_states) # check accept states
+        self.assertEqual(expected_dfa.initial_state, ndfa_determinized.initial_state) # check initial state
+
+        # check states and alphabet from DFA generated
+        self.assertSetEqual(expected_dfa.get_states(), ndfa_determinized.get_states())
+        self.assertSetEqual(expected_dfa.get_alphabet(), ndfa_determinized.get_alphabet())
+
+        # validate sentences 'aaa' and 'ababab'
+        self.assertTrue(ndfa_determinized.validate_sentence('ababab'))
+        self.assertTrue(ndfa_determinized.validate_sentence('aaa'))
+
+        # reject sentence 'aaabab'
+        self.assertFalse(ndfa_determinized.validate_sentence('aaabab'))
+
+    def test_determinization_epsilon_NDFA(self):
+        """new delta table determinate
+            transform this epsilon-NDFA to compatible DFA
+            L = { #a=pair xor #b=par }
+
+
+            delta |    a   |   b   |   &   |             new_delta  |   a  |   b  |
+            ------|--------|-------|-------|           -------------|------|------|
+             ->q0 |    -   |   -   | q1,q3 |      \    ->*{q0,q1,q3}| {q2} | {q4} |
+              *q1 |   q2   |   -   |   -   |   ----\            {q2}| {q1} |   -  |
+               q2 |   q1   |   -   |   -   |   ----/            {q4}|   -  | {q3} |
+              *q3 |    -   |   q4  |   -   |      /            *{q1}| {q2} |   -  |
+               q4 |    -   |   q3  |   -   |                   *{q3}|   -  | {q4} |
+            -------------------------------|           ---------------------------|
+
+        """
+        epsilon_ndfa = self.create_epsilon_ndfa()
+        epsilon_ndfa_determinized = epsilon_ndfa.determinization()
+
+        self.assertIsInstance(epsilon_ndfa_determinized, DFA)
+
+        expected_dfa = self.expected_dfa_epsilon_free()
+        self.assertDictEqual(expected_dfa.delta, epsilon_ndfa_determinized.delta) # check delta transitions
+        self.assertSetEqual(expected_dfa.accept_states, epsilon_ndfa_determinized.accept_states) # check accept states
+        self.assertEqual(expected_dfa.initial_state, epsilon_ndfa_determinized.initial_state) # check initial state
+
+        # check states and alphabet from DFA generated
+        self.assertSetEqual(expected_dfa.get_states(), epsilon_ndfa_determinized.get_states())
+        self.assertSetEqual(expected_dfa.get_alphabet(), epsilon_ndfa_determinized.get_alphabet())
+
+        # validate sentences 'empty('')', 'aa' and 'bbbb'
+        self.assertTrue(epsilon_ndfa_determinized.validate_sentence(''))
+        self.assertTrue(epsilon_ndfa_determinized.validate_sentence('aa'))
+        self.assertTrue(epsilon_ndfa_determinized.validate_sentence('bbbb'))
+
+        # reject sentence 'aaa'
+        self.assertFalse(epsilon_ndfa_determinized.validate_sentence('aaa'))
+
+    def test_epsilon_closure_from_q0(self):
+        epsilon_ndfa = self.create_epsilon_ndfa()
+        _epsilon_closure = epsilon_ndfa.epsilon_closure('q0')
+        expected_closure = {'q0','q1','q3'}
+        self.assertSetEqual(expected_closure, _epsilon_closure)
+
+
 
 if __name__ == '__main__':
     unittest.main()
